bc6475bee9a29bb3ff035e29a0f34fa4cb664c89
Platformarobocza <platformarobocza@gmail.com>
2016-11-07 18:56:58 +0100

    new

---
+++ app.js
@@ -0,0 +1,21 @@
+ var bodySelection = d3.select("body");
+
+ var svgSelection = bodySelection.append("svg")
+       .attr("width", 50)
+       .attr("height", 50);
+
+ var circleSelection = svgSelection.append("circle")
+       .attr("cx", 25)
+       .attr("cy", 25)
+       .attr("r", 25)
+       .style("fill", "purple");
+
+
+
+var theData = [ 1, 2, 3 ]
+
+var p = d3.select("body").selectAll("p")
+  .data(theData)
+  .enter()
+  .append("p")
+  .text(theData);
